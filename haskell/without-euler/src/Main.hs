1ac01fda02c51bf9ae41cd72867e3f9de9c0d2cb
sbh69840 <sbh69840@gmail.com>
2023-05-17 18:58:05 +0530

    without-euler redis bench

--- haskell/without-euler/src/Main.hs
+++ haskell/without-euler/src/Main.hs
@@ -1,70 +1,26 @@
-{-# LANGUAGE ScopedTypeVariables #-}
-{-# LANGUAGE DataKinds #-}
-{-# LANGUAGE TypeOperators #-}
-{-# LANGUAGE DeriveAnyClass #-}
-{-# LANGUAGE DeriveGeneric #-}
 {-# LANGUAGE OverloadedStrings #-}
-{-# LANGUAGE NoImplicitPrelude #-}
-module Main where
-
+import Database.Redis (connect, defaultConnectInfo, runRedis, incr, get, set)
+import Control.Monad (replicateM)
 import Control.Monad.IO.Class (liftIO)
+import System.CPUTime (getCPUTime)
 
-import Data.Text (Text)
-import Data.Aeson (ToJSON)
-import Data.Maybe (fromMaybe)
-import GHC.Generics (Generic)
+iter = 100000
 
-import qualified Data.HashMap.Strict as HM
-
-import Network.Wai.Handler.Warp (run)
-import Relude
-import Servant
-
--- Response for /get_foo 
-data FooMessage = FooMessage
-  { msg :: Text
-  }
-  deriving (Show, Eq, Ord, Generic, ToJSON)
-
-type FooAPI =
-  ( "get_foo"
-    :> QueryParam "log" Int
-    :> Get '[JSON] FooMessage
-  )
-
-fooAPI :: Proxy FooAPI
-fooAPI = Proxy
-
-
-fooServer :: Server FooAPI
-fooServer = getFoo
-
-
--- Warp HTTP application
-fooApp :: Application
-fooApp = serve fooAPI fooServer
-
-
-fooFlow :: Int -> IO FooMessage
-fooFlow should_log = do
-  case should_log of
-    0 ->
-      pure $ FooMessage $ fromMaybe "default" (HM.lookup 1 myHashMap)
-    otherwise -> do
-      print $ show $ HM.toList myHashMap
-      pure $ FooMessage "done"
+main = do
+  conn <- connect defaultConnectInfo
+  let
+    key = "foo"
+    runUnPipelined = show <$> do
+      runRedis conn (set key "1")
+      replicateM iter $ do
+        runRedis conn $ incr key
+        runRedis conn $ get key
+  tNoPipe <- deltaT $ runUnPipelined >>= print
+  putStrLn $ (show tNoPipe) ++ " micro secs"
   where
-    myHashMap :: HM.HashMap Int Text
-    myHashMap = HM.fromList [(i, show i) | i <- [1..20000000]]
-
-getFoo :: Maybe Int -> Handler FooMessage
-getFoo should_log = liftIO $ fooFlow (fromMaybe 0 should_log)
-
--- Foo server entry point
-runFooServer :: Int -> IO ()
-runFooServer port = do
-  putStrLn $ "Starting Foo Server on port " ++ show port ++ "..."
-  run port fooApp
-
-main :: IO ()
-main = runFooServer 8081
+    deltaT redis = do
+        start <- liftIO getCPUTime
+        _ <- redis
+        end <- liftIO getCPUTime
+        let inMicroSeconds = fromIntegral (end - start) / 1000000 :: Double
+        return $  inMicroSeconds / (fromIntegral iter)
